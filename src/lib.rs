c8cc6b2cd757941e8c79ae2c747a4c109efa7c6e
Eray Erdin <eraygezer.94@gmail.com>
2024-04-27 00:07:13 +0300

    remove redundant ControlPlugin

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,5 @@
 // You should have received a copy of the GNU General Public License
 // along with Oxidized Pixel Dungeon.  If not, see <https://www.gnu.org/licenses/>.
 
-mod control;
 pub mod core;
 mod grid;
