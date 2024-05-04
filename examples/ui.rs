3a096918c72007dfb14e049a569ff98113e165bf
Eray Erdin <eraygezer.94@gmail.com>
2024-05-04 13:15:59 +0300

    init ui example

---
+++ examples/ui.rs
@@ -0,0 +1,32 @@
+// Copyright (C) 2024 Eray Erdin
+//
+// This file is part of Oxidized Pixel Dungeon.
+//
+// Oxidized Pixel Dungeon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Oxidized Pixel Dungeon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Oxidized Pixel Dungeon.  If not, see <https://www.gnu.org/licenses/>.
+
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use oxidized_pixel_dungeon::core::CorePlugin;
+
+fn main() {
+    let mut app = App::new();
+
+    app.add_plugins(CorePlugin);
+
+    if cfg!(debug_assertions) {
+        app.add_plugins(WorldInspectorPlugin::new());
+    }
+
+    app.run();
+}
