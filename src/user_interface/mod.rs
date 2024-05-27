5566aa43ad0601db41d2a6102cd7ea09bd9b09a9
Eray Erdin <eraygezer.94@gmail.com>
2024-05-27 22:39:49 +0300

    init InteractableButtonBundle

--- src/user_interface/mod.rs
+++ src/user_interface/mod.rs
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Oxidized Pixel Dungeon.  If not, see <https://www.gnu.org/licenses/>.
 
+pub mod bundles;
 pub mod components;
 pub mod interactions;
 pub mod resources;
