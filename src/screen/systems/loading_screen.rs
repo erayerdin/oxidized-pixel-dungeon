fdf38f473a0fd57eba2aa48301e45427e5bb63f6
Eray Erdin <eraygezer.94@gmail.com>
2026-01-09 22:58:39 +0300

    wip: init loading_screen_system

--- src/screen/systems/loading_screen.rs
+++ src/screen/systems/loading_screen.rs
@@ -15,18 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Oxidized Pixel Dungeon.  If not, see <https://www.gnu.org/licenses/>.
 
-mod states;
-mod systems;
-
-use bevy::prelude::*;
-
-pub(super) struct ScreenPlugin;
-
-impl Plugin for ScreenPlugin {
-    fn build(&self, app: &mut App) {
-        app.init_state::<states::ScreenState>().add_systems(
-            Update,
-            systems::loading_screen_system.run_if(in_state(states::ScreenState::Loading)),
-        );
-    }
+pub fn loading_screen_system() {
+    todo!()
 }
