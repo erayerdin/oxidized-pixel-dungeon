770db7227c8af191ccedc721b1d956fa815d00c9
Eray Erdin <eraygezer.94@gmail.com>
2026-01-10 00:46:11 +0300

    impl dungeon_render_system

--- src/dungeon/systems/dungeon_render.rs
+++ src/dungeon/systems/dungeon_render.rs
@@ -15,6 +15,41 @@
 // You should have received a copy of the GNU General Public License
 // along with Oxidized Pixel Dungeon.  If not, see <https://www.gnu.org/licenses/>.
 
-pub fn dungeon_render_system() {
-    todo!("Not implemented yet");
+use bevy::{platform::collections::Equivalent, prelude::*};
+
+use crate::{
+    GameVariant,
+    dungeon::{components::Grid, constants::GRID_SIZE},
+};
+
+pub fn dungeon_render_system(
+    q1: Query<(Entity, &Grid)>,
+    variant: Res<GameVariant>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+) {
+    // TODO: parallelize
+    for (entity, grid) in q1.iter() {
+        let (x, y) = grid.xy();
+
+        commands.entity(entity).insert(Transform::from_xyz(
+            (x * GRID_SIZE as u16) as f32,
+            (y * GRID_SIZE as u16) as f32,
+            // BUG: this might cause z-fighting
+            0.0,
+        ));
+
+        if variant.equivalent(&GameVariant::ExampleTiles) {
+            commands.entity(entity).insert((
+                Mesh2d(meshes.add(Rectangle::new(GRID_SIZE as f32, GRID_SIZE as f32))),
+                MeshMaterial2d(materials.add(Color::Srgba(Srgba {
+                    red: 0.25,
+                    green: 0.25,
+                    blue: 0.25,
+                    alpha: 0.8,
+                }))),
+            ));
+        }
+    }
 }
